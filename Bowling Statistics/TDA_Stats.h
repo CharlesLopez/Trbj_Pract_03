e8775e8d6e76059bc6aec76d01578826b1fe7502
Francisco P. <pirra_23@hotmail.com>
2013-06-21 01:44:20 -0300

    Varios

--- Bowling Statistics/TDA_Stats.h
+++ Bowling Statistics/TDA_Stats.h
@@ -1,13 +1,13 @@
 #ifndef __TDA_Stats__
 #define __TDA_Stats__
-
+    #if !defined(MAYOR)
+        #define ERROR 1
+    #endif
 
 
 typedef struct{
-    union{
-        char Liga[20];      /* Sirve tanto para mostrar ligas o players, depende como se inicialize*/
-        char Player[20];
-    };
+    char Liga[20];
+    char Player[20];
     int Promedio;
     int Porcentajes[4];
     int Creado;
@@ -17,8 +17,9 @@ typedef struct{
 int OutStatsCrear(TDA_Stats * Stats);
 /* Recibe un TDA Stats, lo inicializa y lo deja listo para ser utilizado*/
 
-int OutStatsCargar(TDA_Stats * Stats, char nombre[20], int promedio, int strikes, int spares, int canaletas, int otros);
-/* Recibe un TDA Stats creado, el nombre del jugador o liga, el promedio del puntaje, y el porcentaje de strikes, spares, canaletas y otros
+int OutStatsCargar(TDA_Stats * Stats, int Tipo ,char nombre[20], int promedio, int strikes, int spares, int canaletas, int otros);
+/* Recibe un TDA Stats creado, el nombre del jugador o liga que segun si es LIGA (1) y JUGADOR (2) en el parametro tipo
+    , el promedio del puntaje, y el porcentaje de strikes, spares, canaletas y otros
     Carga todo en el TDA Stats y devuelve RES_OK si todo funciono correctamente*/
 
 int OutStatsMostrar(TDA_Stats * Stats);
