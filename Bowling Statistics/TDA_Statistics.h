99802e04b9ef538106f3319ebbe71205764beecd
Francisco P. <pirra_23@hotmail.com>
2013-06-20 21:18:35 -0300

    Agregado del destruir y detalles varios

--- Bowling Statistics/TDA_Statistics.h
+++ Bowling Statistics/TDA_Statistics.h
@@ -28,16 +28,16 @@
 typedef struct{
     TAB Samples;
     TAB Players;
-    TListaSimple Lista;
-}TDA_Statistics
+    TDA_Lista_Ordenada Lista;
+}TDA_Statistics;
 
-int StatsCrear(TDA_Statistics* Statistics)
+int StatsCrear(TDA_Statistics* Statistics);
 /* Recibe un TDA_Statistics, inicializa la estructura dejándola lista para utilizarse.
 Devuelve un número entero como código de error o de exito.
 En caso de que todo resulte como es esperado, se deberá devolver RES_OK definido como 0.
 */
 
-int StatsContarMuestra(TDA_Statistics* Statistics, const TDA_Score * Muestra, const char* Liga)
+int StatsContarMuestra(TDA_Statistics* Statistics, const TDA_Score * Muestra, const char* Liga);
 /* Recibe el TDA_Statistics previamente creado, un TDA_Score para contabilizar y la liga a la que corresponde dicho score.
 Los valores de retorno para esta primitiva deberán ser:
 RES_OK: (0) Se agregó la nueva muestra correctamente al tda.
@@ -47,7 +47,7 @@ RES_ERROR_REGISTRAR_MUESTRA: (2) No se pudo contabilizar la muestra porque no fu
                                 (en caso de que falle la inserción en el AB samples)
 */
 
-int StatsObtenerEstadisticasJugador(TDA_Statistics* Statistics, const char* Jugador, TDA_Stats* Stats)
+int StatsObtenerEstadisticasJugador(TDA_Statistics* Statistics, const char* Jugador, TDA_Stats* Stats);
 /* Recibe el TDA_Statistics creado y cargado, el jugador cuyas estadísticas se desean, y un parámetro de salida stats,
 donde se pondrá el resultado del cálculo de estadísticas. Calcula las estadísticas correspondientes al jugador indicado por parámetro y carga los valores en stats.
 El valor de retorno de la primitiva deberá indicar si pudo obtener las estadísticas (en caso de que existan datos del jugador en el tda) o,
@@ -55,7 +55,7 @@ en caso de errores, cuál fue el problema. Queda a cargo del grupo definir los er
 pero se requiere que los errores se indiquen con valores enteros negativos.
 */
 
-int StatsObtenerEstadisticasLiga(TDA_Statistics* Statistics, const char* Liga, TDA_Stats* Stats)
+int StatsObtenerEstadisticasLiga(TDA_Statistics* Statistics, const char* Liga, TDA_Stats* Stats);
 /* Recibe el TDA_Statistics creado y cargado, la liga cuyas estadísticas se desean, y un parámetro de salida stats,
 donde se pondrá el resultado del cálculo de estadísticas.
 Calcula las estadísticas correspondientes a la liga indicada por parámetro y carga los valores en stats.
@@ -64,4 +64,7 @@ en caso de errores, cuál fue el problema.
 Queda a cargo del grupo definir los errores posibles y sus valores de retorno definidos, pero se requiere que los
 errores se indiquen con valores enteros negativos.
 */
+
+int DestruirStats(TDA_Statistics* Statistics);
+/* libera los recursos utilizados por el Statistics y deja inutilizable el TDA */
 #endif
